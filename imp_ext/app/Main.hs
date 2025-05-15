f241c2fdb9033f5f748b8d3791e2934cde35870c
Cosmin <cosmin.calinov@yahoo.com>
2025-05-15 11:07:30 +0300

    Added a TODO

--- imp_ext/app/Main.hs
+++ imp_ext/app/Main.hs
@@ -8,6 +8,7 @@ import Control.Parallel.Strategies
 -- TODO: testare paralelism
 -- TODO: poate adaugare mai multor tipuri de date (char, string, float)
 -- TODO: compilare si rulare cu ghc pt a specifica nr de procese
+-- TODO: impartirea in fisiere/module
 
 newtype Variabila = Var String deriving(Eq, Ord, Show)
 
