e92591ea44cf0f9ea1d3ba3ecc707f840c9ebc23
Cosmin <cosminov.calin@gmail.com>
2026-03-09 12:20:06 +0200

    Updated DOC

--- imp_ext/src/Stmt.hs
+++ imp_ext/src/Stmt.hs
@@ -26,7 +26,7 @@ instance Arbitrary Stmt where
                         , (2, Block <$> resize (n `div` 2) arbitrary)
                         , (2, Par <$> resize (n `div` 2) arbitrary)
                         ]
-        where 
+        where
           arbHalf = arb (n `div` 2)
           -- Bounded Generation
           -- Helper to generate loops that are guaranteed to terminate
@@ -34,22 +34,22 @@ instance Arbitrary Stmt where
           safeWhile _ = do
             originalCond <- resize 2 arbitrary -- simple condition
             -- Force body to avoid nesting
-            body <- resize 2 (listOf1 (oneof [Assign <$> arbitrary <*> arbitrary])) 
-              >>= return . Block 
-            
+            body <- resize 2 (listOf1 (Assign <$> arbitrary <*> arbitrary))
+              >>= return . Block
+
             limitVal <- choose (1, 3 :: Int)        -- limit iterations to very small number
             limitSuffix <- choose (1, 1000 :: Int)  -- random suffix to avoid variable collision
-            
+
             let limitVar = Var ("__limit_" ++ show limitSuffix)
             let initLimit = Assign limitVar (EInt limitVal)
             let checkLimit = BGt (EVar limitVar) (EInt 0)
             let decrLimit = Assign limitVar (ESub (EVar limitVar) (EInt 1))
-            
+
             -- Combine original condition with iteration limit
             let safeCond = BAnd originalCond checkLimit
             -- Add decrement to loop body
             let safeBody = Seq body decrLimit
-            
+
             return $ Seq initLimit (While safeCond safeBody)
 
 -- schimba MyState, adaugand sau inlocuind variabile
@@ -88,7 +88,6 @@ stmt sigma (Block (stmt1 : rest)) =
     in stmt sigma2 (Block rest)
 stmt sigma (Par stmts) =
     let partials = withStrategy (parList rpar $!) (map (stmt sigma) stmts)
-    -- TODO: verifica daca modificarile sunt consistente sau evaluare de expresii in paralel
     in foldl myMerge (List []) partials -- facem merge la fiecare MyState
 
 removeDups :: [(Variable, Value)] -> [(Variable, Value)]
